40e1f18d316f553dc9ed8e9eaa05926a68205df7
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2023-12-30 00:49:09 +0800

    Create ItemPriceList

---
+++ lib/components/item_price_list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shop_price_list/components/item_card.dart';
+import 'package:shop_price_list/db/database.dart';
+
+class ItemPriceList extends StatefulWidget {
+  final Future<List<Item>> items;
+  final VoidCallback refresh;
+
+  const ItemPriceList({Key? key, required this.items, required this.refresh}) : super(key: key);
+
+  @override
+  _ItemPriceListState createState() => _ItemPriceListState();
+}
+
+class _ItemPriceListState extends State<ItemPriceList> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: widget.items, // Use the Future for the builder
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Expanded(
+              child: Center(
+            child: CircularProgressIndicator(),
+          ));
+        }
+
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          return Expanded(
+            child: ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ItemCard(
+                  id: snapshot.data![index].id,
+                  name: snapshot.data![index].name,
+                  price: snapshot.data![index].price,
+                  refresh: widget.refresh,
+                );
+              },
+            ),
+          );
+        }
+
+        return const Center(
+          child: Text("No Data"),
+        );
+      },
+    );
+  }
+}
