85d0332fa0487d478f0ece5fb3c47cd553a2cbe5
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-08 23:57:36 +0800

    Create TotalPriceInCart

---
+++ lib/components/total_price_in_cart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_price_list/bloc/cart_bloc/cart_bloc.dart';
+import 'package:shop_price_list/utils/format_currency.dart';
+
+class TotalPriceInCart extends StatefulWidget {
+  const TotalPriceInCart({Key? key}) : super(key: key);
+
+  @override
+  _TotalPriceInCardState createState() => _TotalPriceInCardState();
+}
+
+class _TotalPriceInCardState extends State<TotalPriceInCart> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CartBloc, int>(
+      builder: (context, state) {
+        return Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                color: Colors.black, // border color
+                width: 1.0, // border width
+              ),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Total",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                formatCurrency(state),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
