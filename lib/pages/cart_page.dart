a92bae3778b42796e2740e22937eeb1c47bb192c
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-08 23:58:07 +0800

    Create CartPage

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shop_price_list/components/item_list_in_cart.dart';
+import 'package:shop_price_list/components/total_price_in_cart.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -10,8 +12,12 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("CartPage"),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      child: Column(children: [
+        ItemListInCart(),
+        TotalPriceInCart(),
+      ]),
     );
   }
 }
