9df480946e53f0f8ae3ef5b34ec2604a6e11d134
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-02-08 23:58:32 +0800

    Add CartBloc into the providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_price_list/bloc/cart_bloc/cart_bloc.dart';
 import 'package:shop_price_list/bloc/items_bloc/items_bloc.dart';
 import 'package:shop_price_list/bloc/search_bloc/search_bloc.dart';
 import 'package:shop_price_list/pages/main_page.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<ItemsBloc>(create: (context) => ItemsBloc()..add(FetchItemsEvent())),
         BlocProvider<SearchBloc>(create: (context) => SearchBloc()),
+        BlocProvider<CartBloc>(create: (context) => CartBloc()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
